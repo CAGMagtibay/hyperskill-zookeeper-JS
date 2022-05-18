6fa3fccb83af36b11da697e85306bb25cf25038d
Christopher Magtibay <41831788+CAGMagtibay@users.noreply.github.com>
2022-05-18 12:58:09 -0400

    Create answer.js

---
+++ stage-1/answer.js
@@ -0,0 +1,5 @@
+console.log("I love animals!");
+console.log("Let's check on the animals...");
+console.log("The deer looks fine.");
+console.log("The bat looks happy.");
+console.log("The lion looks healthy.");
